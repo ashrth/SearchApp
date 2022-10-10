8d76b32f05bd4a10f57c18e0bd0d165025eb4a47
ashrth <aashrrita@gmail.com>
2022-10-10 22:55:35 +0530

    init

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 
 
 const Search = () => {
+  const [isSearch, setIsSearch] = useState(false)
+  const [filteredAds, setFilteredAds] = useState([])
   const [ads, setAds] = useState([])
   const [searchValue, setSearchValue] = useState('')
-  const [searchResult, setSearchResult] = useState([])
+  // const [searchResult, setSearchResult] = useState([])
 
 
   useEffect(() => {
-   
-  }, [])
+    fetch(`http://localhost:5000/ads/${searchValue}`).then((response) => response.json())
+    .then((data)=>{setFilteredAds(data)
+      console.log(data)
+  })
+   }, [])
 
   // const getAds = async () => {
   //   let result = await fetch("http://localhost:5000/ads")
@@ -30,16 +35,36 @@ const Search = () => {
     // if(ads.length){
     //   const res = ads.filter((item) => 
     //  setResult([...res])
+    
 setSearchValue(event.target.value)
   }
 
-const onClickSearch = () => {
-  const res = ads.filter(item => item === searchValue)
-  setSearchResult([...res])
+const handleClick = async (event) => {
+  console.log("hello")
+  setIsSearch(true)
+  
+  const show = await fetch(`http://localhost:5000/ads/${searchValue}`).then((response) => response.json())
+    setFilteredAds(show)
+    console.log("hullo", show)
+  // console.log("searc",searchValue)
+  // console.log(ads[0].name)
+  // const res = ads.filter(item => item.name == searchValue)
+  // console.log(res)
+  // setAds([...res])
+  
 }
 useEffect(() => {
-  fetch("http://localhost:5000/ads").then((response) => response.json()).then((ads)=>{console.log(ads)})
+  fetch("http://localhost:5000/ads").then((response) => response.json())
+  .then((ads)=>{setAds(ads)
+    console.log(ads)
+})
  }, [])
+
+//  useEffect(() => {
+//   fetch(`http://localhost:5000/ads/:${}`).then((response) => response.json())
+//   .then((ads)=>{setAds(ads)
+// })
+//  }, [])
 
 
  return (
@@ -47,27 +72,41 @@ useEffect(() => {
   <h1> SearchApp </h1>
   
   <input type="text" value={searchValue} className="searchadsbox" placeholder="Search Ads" onChange={InputHandle} />
-    <button className="searchbtn" onClick={onClickSearch}>Search</button>
-    {
-      searchResult.length ? (
+  
+    <button className="searchbtn" onClick={handleClick}>Search</button>
+    
+    
+    {!isSearch?
+ads.length ? 
+ads.length && ads.map((item)=>
 <ul>
-  {
-        
-          searchResult.length && searchResult.map((item)=>
-          
             <li key={item._id}> 
 
               <div className="adtitle">{item.name}</div> 
               <div> {item.headline} </div>
 
             </li>
-}
+
         </ul>
-        
+  
   )
-  :(<h2>No Result Found</h2>)
+  :<h2>No Result Found</h2>
+  :filteredAds.length ? 
+  filteredAds.length && filteredAds.map((item)=>
+  <ul>
+              <li key={item._id}> 
+  
+                <div className="adtitle">{item.name}</div> 
+                <div> {item.headline} </div>
+  
+              </li>
+  
+          </ul>
     
-    }
+    )
+    :<h2>No Result Found</h2>
+}
+    
   
   </div>
       )
