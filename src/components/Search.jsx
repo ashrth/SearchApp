4d5f0ab1e82230709633f2054e599f3ffb54f68a
ashrth <aashrrita@gmail.com>
2022-10-10 19:00:54 +0530

    init

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,49 +6,66 @@ import { Link } from 'react-router-dom';
 
 const Search = () => {
   const [ads, setAds] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+//   const [ads, setAds] = useState([])
 
   useEffect(() => {
     getAds()
   }, [])
 
-  const getAds = async () => {
-    let result = await fetch("http://localhost:5000/ads")
-    result = await result.json()
-    setAds([...result])
-  }
+//   const getAds = async () => {
+//     let result = await fetch("http://localhost:5000/ads")
+//     result = await result.json()
+//     setAds([...result])
+//   }
  
   const InputHandle = async (event) => {
-    let key = event.target.value
-    if(result.length){
-      const res = result.filter((item) => 
-     setResult([...res])
+//     let key = event.target.value
+//     if(result.length){
+//       const res = result.filter((key) => 
+//      setResult([...res])
     
-    } 
+//     } 
     
-}
-
-
-
-  return (
-    <div className="ads-list">
-    <h1> SearchApp </h1>
-      <input type="" className="searchadsbox" placeholder="Search Ads" onChange={InputHandle} />
-      {
-      
-         ads.length>0? ads.map((item)=>
-        <li key={item._id}> 
-          
-        <div className="adtitle">{item.name}</div> 
-        <div> {item.headline} </div>
-        
-      </li>
-        )
-        :<h2>No Result Found</h2>
-      
-      }
-    
-    </div>
-  )
-
+setSearchValue(event.target.value)
   }
+
+const onClickSearch = () => {
+  const res = ads.filter(item => item === searchValue)
+  setSearchResult([...res])
+}
+useEffect(() => {
+  fetch("http://localhost:5000/ads").then(response => JSON.parse(response)).then(response => setAds(response))
+ }, [])
+
+
+ return (
+  <div className="ads-list">
+  <h1> SearchApp </h1>
+  
+  <input type="text" value={searchValue} className="searchadsbox" placeholder="Search Ads" onChange={InputHandle} />
+    <button onClick={onClickSearch}>Search</button>
+    {
+      searchResult.length ? (
+        <ul>
+          {
+          searchResult.length && searchResult.map((item)=>
+              <li key={item._id}> 
+
+              <div className="adtitle">{item.name}</div> 
+              <div> {item.headline} </div>
+
+            </li>
+          }
+        </ul>
+      )
+      : (<h2>No Result Found</h2>)
+    
+          }
+  
+  </div>
+      )
+  }
+}
 export default Search
